92037cad02202337c98bd99f22bf456ed20d94cb
ElFirdaous28 <elmokhtarifirdaous@gmail.com>
2025-02-11 14:22:45 +0100

    PostgreSQL connection

--- bootstrap.php
+++ bootstrap.php
@@ -1,5 +1,33 @@
 <?php
 
+// use Illuminate\Database\Capsule\Manager as Capsule;
+
+// require_once __DIR__ . '/vendor/autoload.php';
+// require_once __DIR__ . '/config/config.php';
+// $capsule = new Capsule;
+
+// $capsule->addConnection([
+//    'driver'    => 'mysql',
+//    'host'      => DB_HOST,
+//    'database'  => DB_NAME,
+//    'username'  => DB_USER,
+//    'password'  => DB_PASSWORD,
+// ]);
+
+// $capsule->setAsGlobal();
+// $capsule->bootEloquent();
+
+// // Create migrations table if not exists
+// if (!Capsule::schema()->hasTable('migrations')) {
+//    Capsule::schema()->create('migrations', function ($table) {
+//       $table->increments('id');
+//       $table->string('migration');
+//       $table->integer('batch');
+//       $table->timestamps();
+//    });
+// }
+
+
 use Illuminate\Database\Capsule\Manager as Capsule;
 
 require_once __DIR__ . '/vendor/autoload.php';
@@ -7,11 +35,12 @@ require_once __DIR__ . '/config/config.php';
 $capsule = new Capsule;
 
 $capsule->addConnection([
-   'driver'    => 'mysql',
+   'driver'    => 'pgsql',  // Change to 'pgsql' for PostgreSQL
    'host'      => DB_HOST,
    'database'  => DB_NAME,
    'username'  => DB_USER,
    'password'  => DB_PASSWORD,
+   'port'      => DB_PORT,
 ]);
 
 $capsule->setAsGlobal();
