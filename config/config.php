f2c017167da3d3c019455c20b619436705cea6fc
ElFirdaous28 <elmokhtarifirdaous@gmqil.com>
2025-02-11 14:22:45 +0100

    PostgreSQL connection

--- config/config.php
+++ config/config.php
@@ -16,4 +16,4 @@ define('DB_USER', $_ENV['DB_USER']);
 define('DB_PASSWORD', $_ENV['DB_PASSWORD']);
 define('DB_NAME', $_ENV['DB_NAME']);
 define('DB_DRIVER', $_ENV['DB_DRIVER']);
-define('PORT', $_ENV['PORT']);
+define('DB_PORT', $_ENV['DB_PORT']);
