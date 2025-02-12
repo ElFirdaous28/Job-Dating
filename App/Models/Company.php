9934311591075d4b9c77fd9dfd716aeec461f0e6
samira kibous <kbs130999@gmail.com>
2025-02-12 12:19:16 +0100

    JD-8 affichage des annonces

--- App/Models/Company.php
+++ App/Models/Company.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use App\Core\Model;
 
-class Companies extends Model
+class Company extends Model
 {
     protected $table = 'companies';
     protected $fillable = ["title", "description", "job_category", "company_id"];
