fbf4db89ad9fba455ebd376b5c51f2e1d16f5195
anashm <anashammaoui07@gmail.com>
2025-02-13 17:19:40 +0100

    JD-7 complete update announce using ajax

--- config/routes.php
+++ config/routes.php
@@ -35,6 +35,7 @@ $router->addRoute('POST', '/admin/add_company', [CompanyController::class, 'crea
 // Admin Announecement
 $router -> addRoute("GET","/admin/announcements",[AnnounceController::class, 'annoncements']);
 $router -> addRoute("POST","/admin/announcements/add",[AnnounceController::class, 'create']);
+$router -> addRoute("POST","/admin/announcements/edit",[AnnounceController::class, 'updateAnnounce']);
 
 // Admin companies
 
